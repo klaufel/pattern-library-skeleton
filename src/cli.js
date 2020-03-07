2d139463b31535faa99e121052ad54b3e6923404
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-03-07 19:10:56 +0100

    Add design tokens CLI

---
+++ src/cli.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import getFigma from './design-tokens/getFigma';
+const path = './figma.config.json';
+
+export function cli(args) {
+  //console.log('cli args', args);
+
+  fs.access(path, fs.F_OK, err => {
+    if (err) {
+      console.error('❌');
+      console.error('\x1b[31m Config file was not found!');
+      console.error('\x1b[31m Please, create a `figma.config.json` with a config parameters');
+      return;
+    }
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) throw err;
+      const obj = JSON.parse(data);
+
+      if (!obj.FIGMA_APIKEY) {
+        return console.log('❌  No Figma API Key found');
+      } else if (!obj.FIGMA_ID) {
+        return console.log('❌  No Figma ID found');
+      } else {
+        if (!obj.FIGMA_OUTDIR) {
+          console.log('⚠️ No outdir found, default outdir is `./tokens.json`');
+        }
+        getFigma(obj.FIGMA_APIKEY, obj.FIGMA_ID, obj.FIGMA_OUTDIR || './tokens.json');
+      }
+    });
+  });
+}
