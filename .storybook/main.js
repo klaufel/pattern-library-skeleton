216013adc7b325841abea6f708592dd3a791ee0f
Juan Carlos Ruiz <juancarlos.ruiz@pccomponentes.com>
2020-03-08 16:28:07 +0100

    Update config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-knobs/register'],
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 const theme = create({
   base: 'light',
   brandTitle: 'Patternn library skeleton',
-  brandUrl: 'https://github.com/',
+  brandUrl: 'https://github.com/klaufel/pattern-library-skeleton',
   //brandImage: 'your-image-url',
 });
 
