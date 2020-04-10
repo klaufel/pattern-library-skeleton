7635852e26f84625e921a67fa6e42dcfe8d56983
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-04-10 23:40:10 +0200

    Remove unused components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 // Atoms
-import Button from './components/atoms/button'
-import Heading from './components/atoms/heading'
-import Icon from './components/atoms/icon'
-import Input from './components/atoms/input'
-import Label from './components/atoms/label'
-import Popover from './components/atoms/popover'
+import Button from './components/atoms/Button'
+import Heading from './components/atoms/Heading'
 
 // Molecules
-import Calendar from './components/molecules/calendar'
-import Datepicker from './components/molecules/datepicker'
-import FormField from './components/molecules/formfield'
+import Card from './components/molecules/Card'
 
 export default {
   Button,
   Heading,
-  Icon,
-  Input,
-  Label,
-  Popover,
-  Calendar,
-  Datepicker,
-  FormField
+  Card
 }
