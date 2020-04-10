7da926be1551e760633f902073960234a9b4f9cc
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-04-10 12:58:37 +0200

    Remove comments

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,10 +2,8 @@ import {create} from '@storybook/theming/create'
 
 const theme = create({
   base: 'light',
-  //brandTitle: 'Pattern library skeleton',
   brandTitle: '#zapatillasFromMars👟🚀',
   brandUrl: 'https://github.com/klaufel/pattern-library-skeleton'
-  //brandImage: 'your-image-url',
 })
 
 export default theme
