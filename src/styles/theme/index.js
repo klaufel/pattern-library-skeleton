60c336ed4fa1b9da6208848fb0cb84813fae3db5
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-04-10 17:49:10 +0200

    Update theme with shadows

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -2,12 +2,14 @@ import colors from './colors'
 import typography from './typography'
 import spacing from './spacing'
 import layout from './layout'
+import shadows from './shadows'
 
 const theme = {
   colors,
   typography,
   spacing,
-  layout
+  layout,
+  shadows
 }
 
 export default theme
---
+++ src/styles/theme/shadows.js
@@ -0,0 +1,9 @@
+import tokens from '../../../tokens/json/shadow'
+
+const shadows = {
+  sm: tokens.shadow.sm.value,
+  md: tokens.shadow.md.value,
+  lg: tokens.shadow.lg.value
+}
+
+export default shadows
