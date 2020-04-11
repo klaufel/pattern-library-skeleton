f31bf859405e4c3596c25cc9df14ec7ca6c4be13
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-04-11 14:12:12 +0200

    Update component

--- src/components/molecules/Card/Card.styles.js
+++ src/components/molecules/Card/Card.styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+  max-width: 640px;
+  margin: 40px auto;
   padding: ${props => props.theme.spacing.md};
   border-radius: ${props => props.theme.radius.sm};
   box-shadow: ${props => props.theme.shadows.sm};
